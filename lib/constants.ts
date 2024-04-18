e5967ec3cc99a2fafc3420121c097692b0317242
Will Kuswanto <99236877+wiku-immutable@users.noreply.github.com>
2024-04-18 10:15:41 +1000

    listOrders

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export enum ImmutableRpcUrl {
+  Testnet = "https://rpc.testnet.immutable.com",
+  Mainnet = "https://rpc.immutable.com",
+}
+
+export enum ImmutableChainName {
+  Testnet = "imtbl-zkevm-testnet",
+  Mainnet = "imtbl-zkevm-mainnet",
+}
---
+++ scripts/listOrders.ts
@@ -0,0 +1,25 @@
+import { config, orderbook } from "@imtbl/sdk";
+
+// https://sphere.market/immutable/collection/0x2483CD7f6bdE4cC603cd9587273692791E7B0569
+const CONTRACT_ADDRESS = "0x2483CD7f6bdE4cC603cd9587273692791E7B0569"; 
+
+(async () => {
+  try {
+    const client = new orderbook.Orderbook({
+      baseConfig: {
+        environment: config.Environment.PRODUCTION,
+      },
+    });
+
+    const listOfListings = await client.listListings({
+      sellItemContractAddress: CONTRACT_ADDRESS,
+      status: orderbook.OrderStatusName.ACTIVE,
+      pageSize: 1, // change pageSize to get more responses
+    });
+
+    console.log(listOfListings);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
